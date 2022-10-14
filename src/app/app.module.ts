3eb77004738cf55ecb12f39968973bf9cbe467fd
danielacrapa <danielacrapa@gmail.com>
2022-10-14 17:03:24 -0300

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './Layout/nav/nav.component';
 import { FooterComponent } from './Layout/footer/footer.component';
+import { HeaderComponent } from './Body/header/header.component';
+import { NosotrosComponent } from './Body/nosotros/nosotros.component';
+import { TrabajoComponent } from './Body/trabajo/trabajo.component';
+import { ServiciosComponent } from './Body/servicios/servicios.component';
+import { ContactoComponent } from './Body/contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
+    NosotrosComponent,
+    TrabajoComponent,
+    ServiciosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
